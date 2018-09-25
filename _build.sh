b4f7297b00042c783feaa2d2753a4bd36811dbb9
yuki-toida <yuki.toidax@gmail.com>
2018-09-25 17:09:42 +0900

    bugfix

--- _build.sh
+++ _build.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-if [ "$1" != "dev" ]; then
-  echo "error. \$1 undefined env (dev)"
-  exit 1
-fi
-
 # node process kill
 killall node
 
@@ -14,11 +9,11 @@ yarn deploy
 cd ../
 
 ENV=$1
-REGISTRY=asia.gcr.io/planet-pluto-$ENV
-IMAGE=knowme-$ENV
+REGISTRY=asia.gcr.io/planet-pluto-dev
+IMAGE=knowme
 
 # build and push latest
-gcloud builds submit --config=_cloudbuild-${ENV}.yaml .
+gcloud builds submit --config=_cloudbuild.yaml .
 
 # delete untag
 digest=`gcloud container images list-tags $REGISTRY/$IMAGE --filter='-tags:*' --format='get(digest)'`
